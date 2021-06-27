aecc97f651506a0d71f4570f24537eb197de8839
Olvin <heribb1991@gmail.com>
2021-06-27 23:37:35 +0200

    deploy, change directory

--- server.js
+++ server.js
@@ -2,8 +2,9 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/equipos'));
+console.log(__dirname + '/dist');
+app.use(express.static(__dirname + '/dist/prototipopartidos'));
 app.get('/*', function(req,res) {
   res.sendFile(path.join(__dirname+
-    '/dist/equipos/index.html'));});
+    '/dist/prototipopartidos/index.html'));});
 app.listen(process.env.PORT || 8080);
